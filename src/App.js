4dd7e5d9e6a9733643d2464ab4b05bcf8dc8be01
gogh21 <gogh.phy@gmail.com>
2022-12-10 02:16:02 +0900

    feat: implement function that generate computer numbers

--- src/App.js
+++ src/App.js
@@ -7,6 +7,17 @@ class App {
     let strike = 0;
     let ball = 0;
     let nothing = 0;
+
+    function createComputerNumber() {
+      computer = [];
+      while (computer.length < 3) {
+        let number = MissionUtils.Random.pickNumberInRange(1, 9);
+        if (!computer.includes(number)) {
+          computer.push(number);
+        }
+      }
+      return createInputUserNumber();
+    }
   }
 }
 
