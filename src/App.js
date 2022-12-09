d9c02a9507b602b633e43a7e2a58525c007b98e3
gogh21 <gogh.phy@gmail.com>
2022-12-10 02:12:18 +0900

    feat: declare variables

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    let computer = [];
+    let userNumber = '';
+    let strike = 0;
+    let ball = 0;
+    let nothing = 0;
+  }
 }
 
 module.exports = App;
